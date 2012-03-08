ca5f67e2ead0247f38468ce6ab27629e4a4f6ce4
Manuel Koller <koller@stat.math.ethz.ch>
2012-03-08 08:13:51 +0100

    Reducing the extension to the minimum.

--- extension.js
+++ extension.js
@@ -1,59 +1,13 @@
-// Copyright (C) 2011 R M Yorston
-// Modified by Manuel Koller
+/* -*- mode: js2; js2-basic-offset: 4; indent-tabs-mode: nil -*- */
+
+// Copyright (C) 2011 Manuel Koller
 // Licence: GPLv2+
 
-const Gettext = imports.gettext;
-const Lang = imports.lang;
-
 const Main = imports.ui.main;
-const WindowManager = imports.ui.windowManager;
-
-
-function DummyWorkspaceSwitcherPopup() {
-    this._init();
-}
-
-DummyWorkspaceSwitcherPopup.prototype = {
-    _init: function() {
-    },
-
-    display: function(direction, index) {
-    }
-};
+const WorkspaceSwitcherPopup = imports.ui.workspaceSwitcherPopup;
 
 function main(extensionMeta) {
-    let localePath = extensionMeta.path + '/locale';
-
-    Gettext.bindtextdomain('gnome-shell-frippery', localePath);
-
-    _f = Gettext.domain('gnome-shell-frippery').gettext;
-
-    WindowManager.WindowManager.prototype._showWorkspaceSwitcher =
-    function(shellwm, binding, window, backwards) {
-        if (global.screen.n_workspaces == 1)
-            return;
-
-        if (this._workspaceSwitcherPopup == null)
-            this._workspaceSwitcherPopup = new DummyWorkspaceSwitcherPopup();
-
-        if (binding == 'switch_to_workspace_left')
-            this.actionMoveWorkspaceLeft();
-        else if (binding == 'switch_to_workspace_right')
-            this.actionMoveWorkspaceRight();
-        // up/down would effectively act as synonyms for left/right if we enabled them;
-        // but that could be considered confusing.
-        else if (binding == 'switch_to_workspace_up')
-            this.actionMoveWorkspaceUp();
-        else if (binding == 'switch_to_workspace_down')
-            this.actionMoveWorkspaceDown();
-    };
-
-    WindowManager.WindowManager.prototype._resetKeyBindings = function() {
-        this.setKeybindingHandler('switch_to_workspace_left', Lang.bind(this, this._showWorkspaceSwitcher));
-        this.setKeybindingHandler('switch_to_workspace_right', Lang.bind(this, this._showWorkspaceSwitcher));
-       this.setKeybindingHandler('switch_to_workspace_up', Lang.bind(this, this._showWorkspaceSwitcher));
-        this.setKeybindingHandler('switch_to_workspace_down', Lang.bind(this, this._showWorkspaceSwitcher));
-    };
-
-    Main.wm._resetKeyBindings();
+    WorkspaceSwitcherPopup.WorkspaceSwitcherPopup.prototype._show =
+	function() {
+	};
 }
